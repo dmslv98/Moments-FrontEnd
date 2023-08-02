22aab189319d5d8468b698374a7dc90829d9b1af
= <=>
2023-08-01 21:51:57 -0300

    Configurado o form para cadastro de um Moments

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-new-moment',
   templateUrl: './new-moment.component.html',
   styleUrls: ['./new-moment.component.css']
 })
+
 export class NewMomentComponent {
+  btnText = 'Compartilhar...';
+
+  constructor(){}
+
+  ngOnInit(): void {}
 
 }
